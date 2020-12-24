1b7f3bb6ed60354554767c28e96603096c26f30a
Marciano José da Anunciação <marciano.anunciacao@certsys.com.br>
2020-12-24 11:45:28 -0300

    Started the hangman game

--- game/hangman_game.py
+++ game/hangman_game.py
@@ -3,6 +3,14 @@ def play():
     print("Welcome to Hangman Game!")
     print("************************")
 
+    secret_word = "Banana"
+    hanged = False
+    correct = False
+    while not hanged and not correct:
+        print("Playing")
+
+    print("Game Over")
+
 
 if __name__ == "__main__":
     play()
