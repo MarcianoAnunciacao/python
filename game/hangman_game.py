8408336e58e4b8533f638f9c12829f6b8b63085d
Marciano José da Anunciação <marciano.anunciacao@certsys.com.br>
2020-12-27 19:24:02 -0300

    Added hangman games logic to replace the undescore to the word if user type corret word, added logic to end game if user type wrong the numbers of shot

--- game/hangman_game.py
+++ game/hangman_game.py
@@ -3,11 +3,35 @@ def play():
     print("Welcome to Hangman Game!")
     print("************************")
 
-    secret_word = "Banana"
+    secret_word = "Banana".upper()
+    right_words = ["_" for word in secret_word]
     hanged = False
     correct = False
+    errors = 0
+
+    print(right_words)
+
     while not hanged and not correct:
         print("Playing")
+        shot = input("Try a word")
+        shot = shot.strip().upper()
+
+        if shot in secret_word:
+            index = 0
+            for word in secret_word:
+                if shot == word:
+                    right_words[index] = word
+                index += 1
+        else:
+            errors += 1
+
+        hanged = errors == 6
+        correct = "_" not in right_words
+        print(right_words)
+    if correct:
+        print("You Win!")
+    else:
+        print("You Lose!")
 
     print("Game Over")
 
